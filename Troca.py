f65b267a4bd1bd950b0ffc128435678aee1cb76a
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-03-15 16:09:15 -0300

    adicionando exercício

---
+++ Troca.py
@@ -0,0 +1,7 @@
+print("Esse programa inverte os valores de duas variavéis")
+a = input("digite o valor da variável A: ")
+b = input("digite o valor da variável B: ")
+troca = a
+a = b
+b = troca
+print(f"Agora que troquei, a variavél A contém: {a} e a variavél B contém {b}")
