63eec1a22df7e83fdac1bf5a76fa7e4e9b592b41
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-03-28 15:24:44 -0300

    subindo exerćicios

--- RM.py
+++ RM.py
@@ -3,3 +3,5 @@ idade = input("Informe sua idade: ")
 if int (idade) >= 18:
     print(f"Sua participação foi autorizada, aluno de RM {rm}")
     print("Mais informações serão enviadas para seu e-mail cadastrado? ")
+else:
+    print("Sua participação não foi autorizada por causa da idade")
