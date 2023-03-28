778b94f861f992c252224246adbd14ae91bd87a8
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-03-27 21:04:29 -0300

    adicionando teste variavel

---
+++ tiposVariaveis.py
@@ -0,0 +1,6 @@
+ano = 1989
+nome = "Luke SkyWalker"
+saldo = 50.30
+print(f"O tipo de variavel ano é:  {ano}")
+print(f"o tipo da variavel nome é: {nome}")
+print(f"o tipo da variavel saldo é: {saldo}")
