76e9efef708f1a49b7f3ee3ead8378eeefffbb72
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-03-28 15:14:36 -0300

    subindo teste fiap

---
+++ RM.py
@@ -0,0 +1,5 @@
+rm = input("Infome seu RM:")
+idade = input("Informe sua idade: ")
+if int (idade) >= 18:
+    print(f"Sua participação foi autorizada, aluno de RM {rm}")
+    print("Mais informações serão enviadas para seu e-mail cadastrado? ")
