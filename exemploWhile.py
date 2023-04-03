b86bbfaff8ce909f325616ac6b4da9d19a13555f
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-04-03 11:01:34 -0300

    subindo exercícios

---
+++ exemploWhile.py
@@ -0,0 +1,5 @@
+resposta = "0"
+while (resposta != "42"):
+    resposta = input("Qual é a resposta para a vida, o universo e tudo mais? ")
+
+print("Parabéns, você acertou! ")
---
+++ exemploWhile2.py
@@ -0,0 +1,4 @@
+i = 0
+while (i<=10):
+        print(f"Mais uma mensagem com i valendo {i}")
+        i = i + 1
