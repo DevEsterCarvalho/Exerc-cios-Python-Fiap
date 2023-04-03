83a90ce791b98c22b634c7eddfa80289bf0f36fe
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-04-03 18:44:31 -0300

    subindo exercícios

--- funcaoSoma.py
+++ funcaoSoma.py
@@ -1,8 +1,9 @@
-def somar ():
-    valor1 = float(input("Digite o primeiro valor: "))
-    valor2 = float(input("Digite o segundo valor: "))
+def somar (valor1, valor2):
     soma = valor1 +  valor2
     print(soma)
 
 print("Soma dos valores digitados: ")
-somar()
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+somar(valor1, valor2)
--- funcaoVelocidadeMedia.py
+++ funcaoVelocidadeMedia.py
@@ -1,9 +1,10 @@
 
-def calcularVelocidadeMedia():
-    distancia = float(input("Digite a distância percorrida"))
-    tempo = float(input("Digite o tempo da viagem"))
+def calcularVelocidadeMedia(distancia, tempo):
     velocidade_media = distancia/tempo
     print(f"A velocidade média é {velocidade_media} km/h")
 
 #inicio programa principal
-calcularVelocidadeMedia()
+distancia = float(input("Digite a distância percorrida"))
+tempo = float(input("Digite o tempo da viagem"))
+
+calcularVelocidadeMedia(distancia,tempo)
