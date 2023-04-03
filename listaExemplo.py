7a00a70a519b8454b70f04f5ae3b8f42154b2bc5
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-04-03 17:25:23 -0300

    subindo exercicio lista

---
+++ listaExemplo.py
@@ -0,0 +1,13 @@
+#criação de uma lista com os nomes dos Jedi
+jedi = ["Yoda", "Luke", "Obi-Wan", "Anakin"]
+#exibindo um valor específico da lista
+print(jedi[2])
+
+# ou da seguinte forma
+
+#criação de uma lista com os nomes dos Jedi
+jedi = ["Yoda", "Luke", "Obi-Wan", "Anakin"]
+#A variável assumirá cada um dos valores da lista
+for nome in jedi:
+    #para cada volta do loop, exibir o valor assumido
+    print(nome)
