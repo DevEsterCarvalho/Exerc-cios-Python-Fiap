9439351967f097517a8bd3fd136112c2102e9e7a
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-04-04 11:02:47 -0300

    subindo exercício

--- funcaoVelocidadeMedia.py
+++ funcaoVelocidadeMedia.py
@@ -1,10 +1,9 @@
 
 def calcularVelocidadeMedia(distancia, tempo):
     velocidade_media = distancia/tempo
-    print(f"A velocidade média é {velocidade_media} km/h")
+    return velocidade_media
 
 #inicio programa principal
 distancia = float(input("Digite a distância percorrida"))
 tempo = float(input("Digite o tempo da viagem"))
-
-calcularVelocidadeMedia(distancia,tempo)
+print(f"A velocidade média é {calcularVelocidadeMedia(distancia,tempo)} km/h")
