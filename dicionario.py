10993492f0f9a7c6ae159cf73a79287a2c8ab7c9
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-22 17:02:29 -0300

    subindo exercício

---
+++ dicionario.py
@@ -0,0 +1,15 @@
+#criando um dicionário com dados universo Star Wars
+
+dicionário = {"Yoda":"Mestre Jedi",
+              "Mace Windu": "Mestre Jedi",
+              "Anakin Skywalker":"Cavaleiro Jedi",
+              "R2-D2":"Dróide",
+              "Dex":"Balconista"}
+
+#exibindo todos os valores do dicionário
+for valor in dicionário.values():
+    print(valor)
+
+#exibindo todos as chaves do dicionário
+for chave in dicionário.keys():
+    print(chave)
