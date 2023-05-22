d5acab454d6ccf38a15bcf54e6a1e7d45f1c7c8d
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-22 12:03:48 -0300

    adicionando exercícios

---
+++ TesteTamanhoSYSLista.py
@@ -0,0 +1,9 @@
+#importando módulo sys
+
+import sys
+
+#criando uma lista vazia
+
+lista = []
+
+print(f"O objeto lista é do tipo {type(lista)} e tem {sys.getsizeof(lista)} bytes")
---
+++ TesteTupla.py
@@ -0,0 +1,19 @@
+#para exibir a tupla
+
+"""categorias = ("padawan", "knight", "master")
+print (categorias)"""
+
+#para "desempacotar" a tupla
+
+"""categorias = ("padawan", "knight", "master")
+teste1, teste2, teste3 = categorias
+print(teste1)
+print(teste2)
+print(teste3)"""
+
+#para "desempacotar" a tupla usando for
+
+categorias = ("padawan", "knight", "master")
+for categoria in categorias:
+    print(categoria)
+
