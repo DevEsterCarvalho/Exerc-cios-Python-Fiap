9cfa07a8ed5edba0342f75257066faf2c73c9ed6
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-22 11:44:34 -0300

    alterando teste de tamanho com sys

--- TesteTamanhoSYS.py
+++ TesteTamanhoSYS.py
@@ -8,6 +8,6 @@ peso = 61.500
 
 #Exibindo uma mensagem, nome da variável, o tipo (type) e o tamanho (getsizeof)
 
-print("A variável nome é do tipo {} e tem {} bytes".format(type(nome), sys.getsizeof(nome)))
-print("A variável idade é do tipo {} e tem {} bytes".format(type(idade), sys.getsizeof(idade)))
-print("A variável peso é do tipo {} e tem {} bytes".format(type(peso), sys.getsizeof(peso)))
+print(f"A variável nome é do tipo {type(nome)} e tem {sys.getsizeof(nome)} bytes")
+print(f"A variável idade é do tipo {type(idade)} e tem {sys.getsizeof(idade)} bytes")
+print(f"A variável peso é do tipo {type(peso)} e tem {sys.getsizeof(peso)} bytes")
