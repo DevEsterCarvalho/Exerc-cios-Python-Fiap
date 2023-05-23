a610da7948a35e05db0287f5d564c7e97a026e62
DevEsterCarvalho <estercarvalho205@gmail.com>
2023-05-23 14:24:58 -0300

    subindo exercícios

---
+++ TesteFOpen.py
@@ -0,0 +1,6 @@
+#usando a função open para criar um objeto do tipo arquivo
+
+arquivo = open("c:\arquivos\arquivo_de_texto.txt")
+
+#verificando o tipo do objeto arquivo
+print(type(arquivo.read()))
